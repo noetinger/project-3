0dc4aa6228599bf777503ac827a28cdfa9f26cd9
Steven Balerna <stevenbalerna@Stevens-MacBook-Air.local>
2019-09-14 12:50:10 -0400

    image update

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,11 +3,13 @@ import "./style.css";
 
 const Footer = props => (
     <div id="footer" className="bg-dark text-light text-center">
-        <h5><a href="">Ellen Evans</a></h5>
-        <h5><a href="">Steven Balerna</a></h5>
-        <h5> &copy; 2019 Positec Tool Corp</h5>  
-        <h5><a href="">Nick Oetinger</a></h5>
-        <h5><a href="">William Sherrill</a></h5>
+        <h5>
+        <a href="">Ellen Evans</a>
+        <a href="">Steven Balerna</a>
+        <a> &copy; 2019 Positec Tool Corp</a>  
+        <a href="">Nick Oetinger</a>
+        <a href="">William Sherrill</a>
+        </h5>
     </div>
 );
 
