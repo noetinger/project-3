42a4b8ed6580904d10c71133973d249a1a97d144
Steven Balerna <stevenbalerna@Stevens-MacBook-Air.local>
2019-09-14 12:54:47 -0400

    touch-up footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,13 +3,13 @@ import "./style.css";
 
 const Footer = props => (
     <div id="footer" className="bg-dark text-light text-center">
-        <h5>
+        <p>
         <a href="">Ellen Evans</a>
         <a href="">Steven Balerna</a>
         <a> &copy; 2019 Positec Tool Corp</a>  
         <a href="">Nick Oetinger</a>
         <a href="">William Sherrill</a>
-        </h5>
+        </p>
     </div>
 );
 
