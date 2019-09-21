65f61a2afeb59c0a4a9f7bbb4f650c08d5cf9338
Steven Balerna <stevenbalerna@Stevens-MacBook-Air.local>
2019-09-21 10:10:04 -0400

    favicon and navbar changes

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -7,7 +7,7 @@ function Nav (props) {
         
         <img src="https://www.positecgroup.com/assets/positec-logo-cbf8f37124c367e4a97b432a27545d79.png" width="200px" height="100%" className="d-inline-block align-top" alt="Positec">
             </img>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse flex-grow-0" id="navbarSupportedContent">
