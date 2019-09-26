1e1baebccb90d8017d920f46a7165e4d2e36f206
Steven Balerna <stevenbalerna@Stevens-MacBook-Air.local>
2019-09-26 18:50:26 -0400

    added padding to footer and linkedin links

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,11 +3,11 @@ import "./style.css";
 function Footer(props){
     return (
 <div id="footer" className="bg-dark d-flex flex-row d-flex justify-content-between">
-        <a href="" className="dev-name">Ellen Evans</a>
-        <a href="" className="dev-name">Steven Balerna</a>
+        <a href="https://www.linkedin.com/in/ellen-evans-79631ba4/" className="dev-name">Ellen Evans</a>
+        <a href="https://www.linkedin.com/in/steve-balerna-0a108b89/" className="dev-name">Steven Balerna</a>
         <a className="dev-warning text-warning" style={{display: "flex", flexDirection: "column", justifyContent: "center"}}> &copy; 2019 Positec Tool Corp</a>  
-        <a href="" className="dev-name">Nick Oetinger</a>
-        <a href="" className="dev-name">William Sherrill</a>      
+        <a href="https://www.linkedin.com/in/nicholas-oetinger-070456157/" className="dev-name">Nick Oetinger</a>
+        <a href="https://www.linkedin.com/in/william-sherrill-b04b20127/" className="dev-name">William Sherrill</a>      
     </div>
     )
 }
